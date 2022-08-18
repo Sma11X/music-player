af0e716fe8dc7ce18406960da74960f9fe8a5686
FXX <540351143@qq.com>
2022-08-19 01:07:27 +0800

    day2

--- pages/home-video/index.js
+++ pages/home-video/index.js
@@ -7,64 +7,51 @@ Page({
    * 页面的初始数据
    */
   data: {
-    topMVs: []
+    topMVs: [],
+    hasMore: true
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad(options) {
-    getTopMV(0).then(res => {
-      this.setData({ topMVs: res.data.data })
+  onLoad: async function (options) {
+    this.getTopMVData(0)
+  },
+  // 封装网络请求的方法
+  getTopMVData: async function (offset) {
+    // 判断是否请求
+    if (!this.data.hasMore) return
+    // 展示动画
+    wx.showNavigationBarLoading()
+    // 真正请求
+    const res = await getTopMV(offset)
+    let newData = this.data.topMVs
+    offset === 0 ? (newData = res.data) : (newData = newData.concat(res.data))
+    // 设置数据
+    this.setData({ topMVs: newData })
+    this.setData({ hasMore: res.hasMore })
+    wx.hideNavigationBarLoading()
+    if (offset === 0) {
+      wx.stopPullDownRefresh()
+    }
+  },
+  // 封装事件处理的方法
+  handleVideoItemClick: function (event) {
+    const id = event.currentTarget.dataset.item.id
+    // 页面跳转
+    wx.navigateTo({
+      url: '/pages/detail-video/index?id=' + id,
     })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
+  // 其他生命周期回调
+  onPullDownRefresh: async function () {
+    this.getTopMVData(0)
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
+  onReachBottom: async function () {
+    this.getTopMVData(this.data.topMVs.length)
   }
 })
